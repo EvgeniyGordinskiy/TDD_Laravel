72844232105c0cbadd1d9340d961f80754db5699
EvgeniyGordinskiy <evgeniy@gordinskiy.com>
2019-02-11 21:07:49 +0200

    Added layout

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,18 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>tddProject</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h4>Projects</h4>
     <ul>
         @foreach($projects as $project)
             <a href="{{ $project->path() }}">{{$project->title}}</a>
         @endforeach
     </ul>
-</body>
-</html>
+@endsection
