5e84095f3a49bb54fc41f0c3f5f87d44fd104b4b
EvgeniyGordinskiy <evgeniy@gordinskiy.com>
2019-02-17 19:09:21 +0200

    Style card

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,9 +5,12 @@
         <h1 class="mr-auto">Projects</h1>
         <a href="/projects/create">Create New Project</a>
     </div>
-    <ul>
+    <div class="flex">
         @foreach($projects as $project)
-            <li><a href="{{ $project->path() }}">{{$project->title}}</a></li>
+        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+            <h3 class="text-xl font-normal py-4">{{str_limit($project->title,20)}}</h3>
+            <div class="text-grey">{{str_limit($project->description, 100)}}</div>
+        </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
